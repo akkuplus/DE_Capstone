c85a8781ebdf09439050bf74a53944f6be48acbf
marcus <github@akkuplus.33mail.com>
2021-10-19 00:04:26 +0200

    major review / rework of all functions and deletion of get_tables_from_raw_data-function
    Signed-off-by: marcus <github@akkuplus.33mail.com>

--- model/process_inbound.py
+++ model/process_inbound.py
@@ -1,140 +1,102 @@
 import os
 import pandas as pd
 import pyspark.sql.functions as F
+from pyspark.sql.types import StructType, StructField, DoubleType, StringType
 
-from model import helper
-
-
+from model import helper, analyze
 
 
 def process_immoscout_data(spark, input_path, output_path):
     """
-    Process the apartment rental data files in "immoscout" subdirectory
-     and derive tables.
+    Process the file containing data for apartment rentals in the given path,
+    derive tables and write these tables as parquet.
 
-    :param spark: SparkSession
-    :param input_path: Project path for Input data. Assuming subdirectory "immoscout" holds data within csv file.
-    :param output_path: Project path for Output data.
+    :param spark:       Apache Spark session.
+    :param input_path:  Path for Input data.
+    :param output_path: Path for Ouput data.
     """
 
-
     # GET filepath to data
-    input_location = os.path.join(input_path, "immoscout", "immo_data.csv")
+    input_location = os.path.join(input_path, "src_immoscout", "immo_data.csv")
     assert os.path.isfile(input_location), f"Data source {input_location} does not exist"
 
-
     # LOAD Rental data
-    pdDF_rental =  pd.io.parsers.read_csv(input_location, dtype={'geo_plz': 'str',
-                                                                 'telekomHybridUploadSpeed': 'str',
-                                                                 'noParkSpaces': 'str',
-                                                                 'houseNumber': 'str',
-                                                                 'noRooms': 'str',
-                                                                 'thermalChar': 'str',
-                                                                 'floor': 'str',
-                                                                 'telekomUploadSpeed': 'str'})
-
-
+    column_types = {'geo_plz': 'str', 'telekomHybridUploadSpeed': 'str', 'noParkSpaces': 'str', 'houseNumber': 'str',
+                    'noRooms': 'str', 'thermalChar': 'str', 'floor': 'str', 'telekomUploadSpeed': 'str'}
+    pdDF_rental = pd.io.parsers.read_csv(input_location, dtype=column_types)
 
     # ANALYZE data
     regex_pattern = "(\d){5}"
-    bool_index = pdDF_rental["geo_plz"].str.match(regex_pattern)
     bool_index_four_digits = ~pdDF_rental["geo_plz"].str.match(regex_pattern)
     pdDF_rental.loc[ bool_index_four_digits, ["geo_plz", "geo_bln", "geo_krs"]]  # which zipcodes having not five digits
-    bool_index_saxony_in_state = pdDF_rental["geo_bln"].str.contains("Sachsen")
-    pdDF_rental.loc[bool_index_saxony_in_state, ["geo_plz", "geo_bln", "geo_krs"]]  # zipcodes having a leading zero
+    #analyze.show_stats1(spark, pdDF_rental)  # show (intermediate) results
 
+    bool_index_saxony_in_state = pdDF_rental["geo_bln"].str.contains("Sachsen")
+    pdDF_rental.loc[bool_index_saxony_in_state, ["geo_plz", "geo_bln", "geo_krs"]]  # zipcodes state name contains Saxony
 
     # FILL incomplete zipcode
+
     # 1. Add leading zero to zipcode, this sets correct zipcodes
     bool_index = bool_index_four_digits & bool_index_saxony_in_state
-    pdDF_rental.loc[ bool_index, ["geo_plz"]] = pdDF_rental.loc[ bool_index, ["geo_plz"]].apply(lambda row: "0"+row)  # add a leading zero
-    pdDF_rental.loc[ bool_index, ["geo_plz", "geo_bln", "geo_krs"]]
-    # 2. Add trailing 'X' to zipcode. This can lead to incorrect zipcodes. Later we use just the leading two decimals!
+    pdDF_rental.loc[ bool_index, ["geo_plz"]] = pdDF_rental.loc[ bool_index, ["geo_plz"]].apply(lambda row: "0"+row)
+    pdDF_rental.loc[ bool_index, ["geo_plz", "geo_bln", "geo_krs"]]  # show
+
+    # 2. Add trailing 'X' to zipcode.
+    # This can lead to incorrect zipcodes. Later, we just use the leading two decimals!
     bool_index = bool_index_four_digits & ~bool_index_saxony_in_state
-    pdDF_rental.loc[bool_index, ["geo_plz"]] = pdDF_rental.loc[bool_index, ["geo_plz"]].apply(lambda row: row + "X")  # add a leading zero
-    pdDF_rental.loc[bool_index, ["geo_plz", "geo_bln", "geo_krs", "regio1", "regio2", "regio3"]]
-    # E.g., Neuenheim-Heidelberg has zipcode 69120 not the 69110, see https://www.plz-suche.org/de/ortssuche?ort=Neuenheim&submit=+
+    pdDF_rental.loc[bool_index, ["geo_plz"]] = pdDF_rental.loc[bool_index, ["geo_plz"]].apply(lambda row: row + "X")
+    pdDF_rental.loc[bool_index, ["geo_plz", "geo_bln", "geo_krs", "regio1", "regio2", "regio3"]]  # show
+    # E.g., 'Neuenheim-Heidelberg' has correct zipcode 69120 not 69110
+    # see https://www.plz-suche.org/de/ortssuche?ort=Neuenheim&submit=+
 
+    # DROP unrelevant columns
+    pdDF_rental.drop(columns=["description", "facilities"], inplace=True)
 
-    # SET relevant columns
-    pdDF_rental.drop(columns=["description", "facilities",
-                              #"houseNumber","petsAllowed"
-                              ], inplace=True)
-
-
-    # GET Spark datafraem
+    # GET Spark dataframe
     RENTAL_SCHEMA = helper.get_schemas('RENTAL')
     assert set(pdDF_rental.columns) == set(RENTAL_SCHEMA.names), f"Columns of Dataframe and schema do not match:\n " \
         f"{set(RENTAL_SCHEMA.names).symmetric_difference(set(pdDF_rental.columns))}"
     df_rental = spark.createDataFrame(pdDF_rental, schema=RENTAL_SCHEMA)
 
-
-    # EXTRACT to table
-    columns = df_rental.columns
-    helper.extract_to_table(base_table=df_rental, columns=columns, table_name="table_rental", output_path=output_path)
-
-    """
-    # SHOW example
-    print("\nShowing sample of table_rental:")
-    df_rental.show(10, truncate=False)
-    helper.logger.debug("Imported raw data to table_rental")
-
-    # PERSIST data to parquet
-    output_location = os.path.join(output_path, "table_rental.parquet")
-    df_rental.write.mode('overwrite').parquet(output_location)
-    helper.logger.debug("Exported table_rental to parquet")
-
-    """
-
-    # LOCATION data
+    # EXTRACT LOCATION table to parquet
     columns = ["scoutId", "date", "regio1", "regio2", "regio3",
                  "geo_bln", "geo_krs", "geo_plz", "street", "streetPlain", "houseNumber"]
     helper.extract_to_table(base_table=df_rental, columns=columns, table_name="table_rental_location", output_path=output_path)
 
-
-    # PRICE_and_COST data
+    # EXTRACT PRICE_and_COST table to parquet
     columns=["scoutId", "date", "serviceCharge", "newlyConst", "pricetrend",
                 "totalRent", "baseRent", "baseRentRange", "heatingCosts", "electricityBasePrice",
                  "electricityKwhPrice"]
     helper.extract_to_table(base_table=df_rental, columns=columns, table_name="table_rental_price_and_cost", output_path=output_path)
 
-
-    # FEATURE data
-    columns = ["scoutId", "date", "heatingType",
-                 "telekomTvOffer", "telekomHybridUploadSpeed",
-                "telekomUploadSpeed", "balcony", "picturecount",
-                "yearConstructed", "yearConstructedRange", "noParkSpaces",
-                "firingTypes", "hasKitchen", "cellar",
-                "livingSpace", "condition", "interiorQual",
-                "petsAllowed", "lift", "typeOfFlat",
-                "noRooms", "thermalChar", "floor",
-                "numberOfFloors", "noRoomsRange", "garden",
-                "livingSpaceRange",
-                "energyEfficiencyClass", "lastRefurbish"]
+    # EXTRACT FEATURE table to parquet
+    columns = ["scoutId", "date", "heatingType", "telekomTvOffer", "telekomHybridUploadSpeed",
+               "telekomUploadSpeed", "balcony", "picturecount", "yearConstructed", "yearConstructedRange",
+               "noParkSpaces", "firingTypes", "hasKitchen", "cellar", "livingSpace", "condition", "interiorQual",
+               "petsAllowed", "lift", "typeOfFlat", "noRooms", "thermalChar", "floor", "numberOfFloors",
+               "noRoomsRange", "garden", "livingSpaceRange", "energyEfficiencyClass", "lastRefurbish"]
     helper.extract_to_table(base_table=df_rental, columns=columns, table_name="table_rental_feature", output_path=output_path)
-
 
     return
 
 
-
 def process_station_data(spark, input_path, output_path):
     """
-    Process the XXX data files in "XXX" subdirectory.
-    Therewith derive tables XXX.
+    Process the file containing data for stations of public transfer in the given path,
+    derive tables and write these tables as parquet.
 
-    :param spark: SparkSession
-    :param input_path: Project path for Input data. Assuming subdirectory "XXX" holds song data within jsons files.
-    :param output_path: Project path for Ouput data.
+    :param spark:       Apache Spark session.
+    :param input_path:  Path for Input data.
+    :param output_path: Path for Ouput data.
     """
 
     # GET filepath to data
-    input_location = os.path.join(input_path, "opendata-oepnv", "zHV_aktuell_csv.2021-09-17.csv")
+    input_location = os.path.join(input_path, "src_opendata-oepnv", "zHV_aktuell_csv.2021-09-17.csv")
     assert os.path.isfile(input_location), f"Data source {input_location} does not exist"
 
     # LOAD data
     STATION_SCHEMA = helper.get_schemas("STATION")
-    # Alternative with XML: df = spark.read.format('xml').options(rowTag='book').load(input_location)
+    # Alternative with XML, since csv is less complex
 
     df_stations = spark.read \
         .schema(STATION_SCHEMA) \
@@ -147,22 +109,12 @@ def process_station_data(spark, input_path, output_path):
         .option("mode", "PERMISSIVE") \
         .csv(input_location)
 
-    # ADAPT data for Latitude and Longitude
+    # Transform values of latitude and longitude
     df_stations = df_stations.withColumn('Latitude', F.regexp_replace('Latitude', ',', '.'))
     df_stations = df_stations.withColumn('Latitude', df_stations['Latitude'].cast("float"))
     df_stations = df_stations.withColumn('Longitude', F.regexp_replace('Longitude', ',', '.'))
     df_stations = df_stations.withColumn('Longitude', df_stations['Longitude'].cast("float"))
-
-    # SHOW example
-    print("\nShowing sample of df_stations in city of Leipzig:")
-    df_stations.where(F.col("MunicipalityCode") == "14713000").show(10, truncate=True)
-    helper.logger.debug("Imported Station Data")
-
-    # STORE data to parquet
-    name = "table_stations"
-    output_location = os.path.join(output_path, f"{name}.parquet")
-    df_stations.write.mode("overwrite").parquet(output_location)
-    helper.logger.debug(f"Exported {name} to parquet")
+    helper.logger.debug("Imported Station data")
 
     # FIND all (major) stations that have a municipality code
     table_stations_Gemeinden = df_stations.where(df_stations["MunicipalityCode"] != "00000000")
@@ -180,91 +132,60 @@ def process_station_data(spark, input_path, output_path):
         # of DHID and Parent. -> The counts are equal, and each row has identic values in columns DHID and Parent.
         # -> This subset of stations contains major stations.
 
-    # STORE data of (major) stations having municipality code
-    df_stations_in_municipal = table_stations_Gemeinden.select(["SeqNo", "Type", "DHID", "Parent", "Name", "Latitude",
-                                                                "Longitude", "MunicipalityCode", "Municipality"])
-    name = "table_stations_in_municipals"
-    output_location = os.path.join(output_path, f"{name}.parquet")
-    df_stations_in_municipal.write.mode("overwrite").parquet(output_location)
-    assert os.path.exists(output_location), f"Dataset {output_location} does not exist."
-    helper.logger.debug(f"Exported {name} as parquet")
+    # EXTRACT table to parquet
+    columns = ["SeqNo", "Type", "DHID", "Parent", "Name", "Latitude", "Longitude", "MunicipalityCode", "Municipality"]
+    helper.extract_to_table(base_table=table_stations_Gemeinden, columns=columns,
+                            table_name="table_majorstations_in_municipals", output_path=output_path)
 
     return
 
 
-def process_mapping_municipality_to_zip(input_path, output_path):
+def process_mappings(spark, input_path, output_path):
     """
-    Process the XXX data files in "XXX" subdirectory.
-    Therewith derive table XXX.
+    Process the files containing data for mappings (municipality code to zip code, and zip code to coordinates)
+    in the given path, derive tables and write these tables as parquet.
 
-    :param input_path: Project path for Input data. Assuming subdirectory "XXX" holds song data within jsons files.
-    :param output_path: Project path for Ouput data.
-    """
-
-    # EXTRACT mapping
-    column_types = {"AGS": str, "Bezeichnung": str, "PLZ": str}
-    data_location = os.path.join(input_path, "destatis-Gemeindeschluessel", "AuszugGV3QAktuell.xlsx")
-    os.path.exists(data_location)  # sheet "Extract" holds an extract of sheet "Onlineprodukt_Gemeinden"
-    dfpd_mapping_municipal_codes = pd.read_excel(data_location, engine="openpyxl",
-                                     sheet_name="Extract", names=column_types.keys(),
-                                     header=0, dtype=column_types)
-
-    output_location = os.path.join(output_path, "table_Mapping_municipal_code_to_zip.parquet")
-    dfpd_mapping_municipal_codes.to_parquet(output_location, compression="snappy")
-    helper.logger.debug("Exported table_Mapping_municipal_code_to_zip as parquet")
-
-    return
-
-
-def process_mapping_zip_2_coord(spark, input_path, output_path):
-    """
-    Process the XXX data files in "XXX" subdirectory.
-    Therewith derive table XXX.
-
-    :param input_path: Project path for Input data. Assuming subdirectory "XXX" holds song data within jsons files.
-    :param output_path: Project path for Ouput data.
+    :param spark:       Apache Spark session.
+    :param input_path:  Path for Input data.
+    :param output_path: Path for Ouput data.
     """
 
-    # EXTRACT mapping
-    column_types = {"PLZ": str, "lat": float, "lng": float}
-    data_location = os.path.join(input_path, "WZBSocialScienceCenter_plz_geocoord", "plz_geocoord.txt")
-    assert os.path.exists(data_location)
-    table_Mapping_zip_2_coor = spark.read \
+    # 1. IMPORT mapping municipality code -> zip code
+    # GET filepath to data
+    input_location = os.path.join(input_path, "src_destatis-Gemeindeschluessel", "AuszugGV3QAktuell.xlsx")
+    assert os.path.isfile(input_location), f"Data source {input_location} does not exist"
+
+    # LOAD data
+    schema = helper.get_schemas("MAPPING")
+
+    df_mapping_municipal_to_zip = spark.read.format("com.crealytics.spark.excel") \
+        .option("useHeader", "true") \
+        .option("Schema", schema) \
+        .option("dataAddress", f"'Extract'!A1") \
+        .load(input_location)
+
+    # EXTRACT table to parquet
+    columns = df_mapping_municipal_to_zip.columns
+    helper.extract_to_table(base_table=df_mapping_municipal_to_zip, columns=columns,
+                            table_name="table_mapping_municipal_to_zip", output_path=output_path)
+
+    # 2. IMPORT mapping zipcode to geo coordinates
+    # GET filepath to data
+    input_location = os.path.join(input_path, "src_WZBSocialScienceCenter_plz_geocoord", "plz_geocoord.txt")
+    assert os.path.isfile(input_location), f"Data source {input_location} does not exist"
+
+    # LOAD data
+    table_mapping_zip_to_coor = spark.read \
         .option("header", True) \
         .option("multiLine", False) \
         .option("ignoreLeadingWhiteSpace", True) \
         .option("sep", ",") \
         .option("mode", "PERMISSIVE") \
-        .csv(data_location)
+        .csv(input_location)
 
-    output_location = os.path.join(output_path, "table_mapping_zip_2_coor.parquet")
-    table_Mapping_zip_2_coor.write.parquet(output_location)
-    helper.logger.debug("Exported table_Mapping_zip_coor as parquet")
-
-    return
-
-
-def get_tables_from_raw_data(spark, input_path, output_path):
-
-    # Rental data
-    data_location = os.path.join(output_path, "table_rental.parquet")
-    if True: #> not (os.path.exists(data_location)):
-        process_immoscout_data(spark, input_path, output_path)
-    assert os.path.exists(data_location), f"Dataset {data_location} does not exist"
-    helper.logger.debug(f"Dataset {data_location} exists.")
-
-    # Station data
-    data_location = os.path.join(output_path, "table_stations.parquet")
-    if True: #not(os.path.exists(data_location)):
-        process_station_data(spark, input_path, output_path)
-    assert os.path.exists(data_location), f"Dataset {data_location} does not exist"
-    helper.logger.debug(f"Dataset {data_location} exists.")
-
-    # Mapping municipal code to zip code
-    data_location = os.path.join(output_path, "table_Mapping_municipal_code_to_zip.parquet")
-    if True: #not(os.path.exists(data_location)):
-        process_mapping_municipality_to_zip(input_path, output_path)
-    assert os.path.exists(data_location), f"Dataset {data_location} does not exist"
-    helper.logger.debug(f"Dataset {data_location} exists.")
+    # EXTRACT table to parquet
+    columns = table_mapping_zip_to_coor.columns
+    helper.extract_to_table(base_table=table_mapping_zip_to_coor, columns=columns,
+                            table_name="table_mapping_zip_to_coor", output_path=output_path)
 
     return
