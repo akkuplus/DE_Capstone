bc6ec343e624442c7c080fec71e397e3617e6b8e
marcus <github@akkuplus.33mail.com>
2022-01-23 17:33:27 +0100

    updates pip packages and synced with requirments.txt file
    Signed-off-by: marcus <github@akkuplus.33mail.com>

---
+++ emr_bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo pip install -U \
+  openpyxl \
+  pyarrow \
+  pyspark~=2.4.3 \
+  boto3~=1.18.64 \
+  requests~=2.26.0 \
+  s3fs~=2021.10.1 \
+  pandas~=1.3.3  \
+  geopy~=2.2.0 \
+  numpy~=1.21.2 \
+  scipy~=1.7.2 \
+  scikit-learn~=1.0.1 \
+  folium~=0.12.1.post1 \
+  plotly~=5.4.0 \
+  dash~=2.0.0 \
+  Flask~=2.0.2
