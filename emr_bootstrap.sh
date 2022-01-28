2c13012034c6a11d56f3432fafdb59364f6b204c
marcus <github@akkuplus.33mail.com>
2022-01-28 21:48:46 +0100

    updated script
    - added amazon corretto
    - added pip3
    - updated pip module list
    
    Signed-off-by: marcus <github@akkuplus.33mail.com>

--- emr_bootstrap.sh
+++ emr_bootstrap.sh
@@ -1,17 +1,9 @@
-#!/bin/bash
-sudo pip install -U \
-  openpyxl \
-  pyarrow \
-  pyspark~=2.4.3 \
-  boto3~=1.18.64 \
-  requests~=2.26.0 \
-  s3fs~=2021.10.1 \
-  pandas~=1.3.3  \
-  geopy~=2.2.0 \
-  numpy~=1.21.2 \
-  scipy~=1.7.2 \
-  scikit-learn~=1.0.1 \
-  folium~=0.12.1.post1 \
-  plotly~=5.4.0 \
-  dash~=2.0.0 \
-  Flask~=2.0.2
+#!/bin/bash -xe
+
+sudo amazon-linux-extras enable corretto8
+sudo yum install java-1.8.0-amazon-corretto -y
+
+sudo yum –y install python3
+sudo yum –y install python3-pip
+
+sudo pip3 install -U pyarrow pyspark~=2.4.3 requests~=2.26.0 s3fs~=2021.10.1 pandas~=1.3.3 numpy~=1.21.2 scipy~=1.7.2 scikit-learn~=1.0.1 folium~=0.12.1.post1 plotly~=5.4.0 Flask~=2.0.2
